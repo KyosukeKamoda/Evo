eae053f5144d5c0d5bde7d4e7a7a681d6e9852cd
KyosukeKamoda <67621728+KyosukeKamoda@users.noreply.github.com>
2020-12-10 20:20:32 +0900

    first commit

---
+++ Assets/Scripts/Move_Impulse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Impulse : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Vector3 Axis;
+    public float Magnitude;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = Mathf.Infinity;
+        Force();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Force()
+    {
+        rb.AddForce(Axis * Magnitude, ForceMode.Impulse);
+    }
+}
